6712befad67f701e193939b60f40d0fbf08440d3
Eduardo Loureiro <142263683+Eduardooh92@users.noreply.github.com>
2024-07-04 13:10:48 +0000

    Endpoint Update [PUT] /item/:id implementado

--- index.js
+++ index.js
@@ -46,7 +46,20 @@ app.get('/item/:id', function(req, res) {
   res.send(item)
 })
 
+//Update - [PUT] item/:id
+app.put('/item/:id', function(req, res) {
+  //Acessamos o ID parâmetro de rota
+  const id = req.params.id
 
+  //Acessamos o novoItem no body da requisição
+  const novoItem = req.body.nome
+
+  //Atualizamos esse novoItem na lista, usando o índice
+  lista[id - 1] = novoItem
+
+  //Enviamos uma mensagem de sucesso
+  res.send('Item atualizado com sucesso' + id)
+})
 
 
 app.listen(3000)
